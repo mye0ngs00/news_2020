f977de1a655dc5c7597d7c2af5d8fb501a2e8678
mye0ngs00 <ourendpoint@gmail.com>
2020-06-10 02:26:01 +0900

    Add NewsList.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try{
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=609c7b0693df4df1a31f3569a8b142e3'
-      );
-      setData(response.data);
-    }catch(e){
-      console.log(e);
-    }
-  };
-  
   return (
-    <div>
-      <div>
-        <button onClick = {onClick}>불러오기</button>
-      </div>
-      { data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
+    <NewsList />
   );
 }
 
