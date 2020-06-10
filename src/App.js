35d071565fd3411f541966148b8f26c8db4221a7
mye0ngs00 <ourendpoint@gmail.com>
2020-06-11 02:26:01 +0900

    Add news contents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import NewsList from './components/NewsList';
 
+/*
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try{
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=609c7b0693df4df1a31f3569a8b142e3'
+      );
+      setData(response.data);
+    }catch(e){
+      console.log(e);
+    }
+  };
+  */
 function App() {
   return (
     <NewsList />
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-import NewsItem from './NewsItem';
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import NewsItem from './NewsItem'
+import axios from 'axios'
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -15,25 +16,42 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160'
-};
-
 const NewsList = () => {
+    const [articles, setArticles] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    // once
+    useEffect(() =>{
+        const fetchData = async () => {
+            setLoading(true);
+            try{
+                const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=609c7b0693df4df1a31f3569a8b142e3');
+                setArticles(response.data.articles)
+            }catch(e){
+                console.log(e)
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [])
+
+    if(loading){
+        return <NewsListBlock>대기 중...</NewsListBlock>
+    }
+    // 아직 articles 값이 설정되지 않았을 때
+    if(!articles){
+        return null;
+    }
+
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {
+                articles.map(article => (
+                    <NewsItem key={article.url} article={article} />
+                ))
+            }
         </NewsListBlock>
     )
 }
 
-export default NewsList;
+export default NewsList
