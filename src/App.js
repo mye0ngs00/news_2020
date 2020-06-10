f16e5214f68d29fe7bbe892443471b1c340c28be
mye0ngs00 <ourendpoint@gmail.com>
2020-06-11 03:06:50 +0900

    Update categories

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 import NewsList from './components/NewsList'
 import Categories from './components/Categories'
-/*
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try{
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=609c7b0693df4df1a31f3569a8b142e3'
-      );
-      setData(response.data);
-    }catch(e){
-      console.log(e);
-    }
-  };
-  */
+
 function App() {
+  const [category, setCategory] = useState('all')
+  const onSelect = useCallback( category => setCategory(category), []);
+
   return (
     <>
-      <Categories />
-      <NewsList />
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category}/>
     </>
   );
 }
